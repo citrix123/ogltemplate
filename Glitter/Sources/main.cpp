417b93ba3e1f26dd53e9730dbf0190610393a316
Rahul Jain <Rahul.Jain@amd.com>
2022-11-27 10:47:15 +0530

    glVertexAttribPointer

--- Glitter/Sources/main.cpp
+++ Glitter/Sources/main.cpp
@@ -9,7 +9,84 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main(int argc, char * argv[]) {
+// Shader sources
+const GLchar* vertexSource = R"glsl(
+    #version 150 core
+    in vec2 position;
+    void main()
+    {
+        gl_Position = vec4(position, 0.0, 1.0);
+    }
+)glsl";
+const GLchar* fragmentSource = R"glsl(
+    #version 150 core
+    uniform vec3 triangleColor;
+    out vec4 outColor;
+    void main()
+    {
+        outColor = vec4(triangleColor, 1.0);
+    }
+)glsl";
+
+GLuint createShader(const GLchar* src, GLenum shader)
+{
+    GLuint id = glCreateShader(shader);
+    glShaderSource(id, 1, &src, NULL);
+    glCompileShader(id);
+
+    GLint status;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &status);
+
+    if (status != GL_TRUE)
+    {
+        char buffer[512];
+        glGetShaderInfoLog(id, 512, NULL, buffer);
+        printf("%s", buffer);
+    }
+
+    return id;
+}
+
+
+GLuint createTriangle()
+{
+    GLfloat vertices[] = {
+        0.0f, 0.5f,
+        0.5f, -0.5f,
+        -0.5f, -0.5f
+    };
+    // Create Vertex Array Object
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    GLuint vertexShader = createShader(vertexSource, GL_VERTEX_SHADER);
+    GLuint fragmentShader = createShader(fragmentSource, GL_FRAGMENT_SHADER);
+
+    // Link the vertex and fragment shader into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glBindFragDataLocation(shaderProgram, 0, "outColor");
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    // Specify the layout of the vertex data
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+    return shaderProgram;
+}
+
+int main(int argc, char* argv[]) {
 
     // Load GLFW and Create a Window
     glfwInit();
@@ -31,14 +108,22 @@ int main(int argc, char * argv[]) {
     gladLoadGL();
     fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
 
+    GLuint shaderProg = createTriangle();
+    srand((unsigned)time(NULL));
+
     // Rendering Loop
     while (glfwWindowShouldClose(mWindow) == false) {
         if (glfwGetKey(mWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
             glfwSetWindowShouldClose(mWindow, true);
 
-        // Background Fill Color
-        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
+        // Clear the screen to black
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        GLuint unicolor = glGetUniformLocation(shaderProg, "triangleColor");
+        glUniform3f(unicolor, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX, (float)rand() / RAND_MAX);
+        // Draw a triangle from the 3 vertices
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         // Flip Buffers and Draw
         glfwSwapBuffers(mWindow);
